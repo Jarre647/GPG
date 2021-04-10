965905dd0b589ba53a341c96cd81b1a1921eaf83
Aleksandr <KirpichnikovAG@suek.ru>
2021-04-10 14:50:17 +0700

    output of the received information from bd

---
+++ WebVue/ClientApp/src/components/CreateGrudge.vue
@@ -0,0 +1,15 @@
+﻿<template>
+
+</template>
+<script>
+    export default {
+        name: "CreateGrudge",
+        data() {
+            return {
+                name: "",
+                reason: ""
+            }
+        },
+        
+    }
+</script>
--- WebVue/ClientApp/src/components/Home.vue
+++ WebVue/ClientApp/src/components/Home.vue
@@ -1,16 +1,34 @@
 <template>
-    <div class="hello">  
-
+    <div class="hello">
+        Abusers<br />
+        <tb>
+            <tr>
+                <td>
+                    Name
+                </td>
+                <td>
+                    Reason
+                </td>
+            </tr>
+            <tr v-for="item in grudges" :key="item.id">
+                <td>
+                    {{item.abuserName}}
+                </td>
+                <td>
+                    {{item.reason}}
+                </td>
+            </tr>
+        </tb>
     </div>
 </template>
 
 <script>
-export default {
-  name: 'Home',
-  props: {
-    msg: String
-  }
-}
+    export default {
+        name: 'Home',
+        props: {
+            grudges: Array
+        }
+    }
 </script>
 
 <style scoped>
